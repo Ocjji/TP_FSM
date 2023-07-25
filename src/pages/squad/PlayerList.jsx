31d4564547f1c74e5d248ad03edb521b41a990a1
audrhks29 <audrhks29@gmail.com>
2023-07-25 18:19:16 +0900

    PlayerItem.jsx UPDATE PlayerList.jsx UPDATE

--- src/pages/squad/PlayerList.jsx
+++ src/pages/squad/PlayerList.jsx
@@ -8,11 +8,42 @@ const PlayerList = () => {
     const { playerData, selectPosition } = useSelector(state => state.playerR);
     return (
         <>
-            {selectPosition && <PlayerListStyle>
-                {
-                    playerData.map(item => <PlayerItem key={item.pid} item={item} />)
-                }
-            </PlayerListStyle>
+            {
+                selectPosition &&
+                <PlayerListStyle>
+                    <div className='pls_left'>
+                        <div className='pls_left_top'>
+                            <h2>선택한 선수 포지션 들어가야함</h2>
+                            <div className='searchWrap'>
+                                <input type="text" name="" id="" placeholder='선수를 검색해주세요' />
+                            </div>
+                        </div>
+                        <ul>
+                            {
+                                playerData.map(item => <PlayerItem key={item.pid} item={item} />)
+                            }
+                        </ul>
+                    </div>
+                    <div className='pls_right'>
+                        <div className='pls_right_top'>
+                            <h2>선수정보</h2>
+                            <div className='btnWrap'>
+                                <button>선택</button>
+                            </div>
+                        </div>
+                        <div className='selectPlayerBox'>
+                            <img src='./images/player/KangInLee.png' alt="" />
+                            <p>
+                                <span> No. 1</span>
+                                <strong> 이명박 </strong>
+                                <span><strong>출생</strong>2000.01.01</span>
+                                <span><strong>포지션</strong>MF</span>
+                                <span><strong>키</strong>198cm</span>
+                                <span><strong>몸무게</strong>90kg</span>
+                            </p>
+                        </div>
+                    </div>
+                </PlayerListStyle>
             }
         </>
     );
